dc45a423e371722da8aef2a9e849b499eda0c39b
Vasudev Koneru <vasudev.koneru1@t-mobile.com>
2018-02-25 15:24:49 -0800

    Java Demo

--- src/io/demo/Demo_.java
+++ src/io/demo/Demo_.java
@@ -1,7 +1,11 @@
 package io.demo;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
 public class Demo_ {
 
@@ -15,7 +19,8 @@ public class Demo_ {
         List<String> bList = new ArrayList<>();
 
         // Enchanced for-loop
-        for(String a : bList) {}
+        for (String a : bList) {
+        }
 
         // Autoboxing/unboxing - int, double, long are primitive DT which cannot be inserted into a list/map etc.
         // Java converts these primitives to wrapper calsses.
@@ -27,5 +32,26 @@ public class Demo_ {
 
         // Implements -> interface
         // Extends -> class
+
+
+        // Vector -> Used in threading environment/Supports synchronization
+        Vector<String> myVector = new Vector<>();
+        myVector.add("Addition");
+
+        // LinkedList -> used in scenarios where frequent updation and deletion occurs.
+
+        String[] myArray = {"a", "b", "c", "d", "e"};
+
+        int removeIndex = 1;
+        for (int i = removeIndex; i < myArray.length - 1; i++) {
+            myArray[i] = myArray[i+1];
+        }
+        System.out.println("After removing the second element: "+Arrays.toString(myArray));
+
+        //TODO -> equals() & hashCode() methods
+
+        Model model = new Model("x", "y");
+
+
     }
 }
---
+++ src/io/demo/Model.java
@@ -0,0 +1,15 @@
+package io.demo;
+
+public class Model {
+
+    private String a;
+    private String b;
+
+    public final String abc = "xyz";
+
+    // Constructor based
+    public Model(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+}
